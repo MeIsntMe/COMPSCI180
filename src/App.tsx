24d447754e03ec40e12626de02cb9a5925bc4d4a
Gracelynn <gracelynn13@gmail.com>
2025-09-02 19:29:04 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import HW0 from './HW0/HW0'
 import Home from './Views/Home'
 import backgroundImage from "./assets/Fall-bg.jpg"
@@ -12,13 +12,13 @@ function App() {
       <div className='py-10 px-20 h-full w-full flex flex-col'>
         <Router>
           <nav className="p-4 bg-gray-200 flex gap-4">
-            <Link to="/Home" className="text-blue-600">Home</Link>
+            <Link to="/" className="text-blue-600">Home</Link>
             <Link to="/HW0" className="text-blue-600">HW0</Link>
           </nav>
 
           <div className='bg-beige grow-1 overflow-scroll'>
             <Routes>
-              <Route path="/Home" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/HW0" element={<HW0 />} />
               <Route path="/*" element={<div>Error 404</div>} />
             </Routes>
